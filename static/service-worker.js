82a1dd992a48fce7e5f836a39392d48eceee7320
Daniel J Dominguez <thinking.danield@gmail.com>
2019-12-19 21:56:54 -0700

    Fixed spacing

--- static/service-worker.js
+++ static/service-worker.js
@@ -2,43 +2,42 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
- var CACHE_NAME = 'dominguezd-v1.0'
+var CACHE_NAME = 'dominguezd-v1.0'
 
- var URLS = [
-	 '/sass/main.min.css',
-	 '/js/script.js',
-	 '/images/mountain.svg',
-	 '/images/balloon.svg',
-	 '/',
-	 '/android-chrome-192x192.png',
-	 '/android-chrome-512x512.png',
-	 '/apple-touch-icon.png',
-	 '/favicon.ico',
-	 '/favicon-16x16.png',
-	 '/favicon-32x32.png',
-	 '/favicon.svg',
-	 '/manifest.json',
-	 '/mstile-150x150.png',
-	 '/safari-pinned-tab.svg'
- ]
- 
- this.addEventListener('install', function (event) {
-	 event.waitUntil(
-		 caches.open(CACHE_NAME).then(function (cache) {
-			 return cache.addAll(URLS);
-		 })
-	 );
- });
- 
- this.addEventListener('fetch', function (event) {
-	 event.respondWith(
-		 caches.match(event.request).then(function (response) {
-			 if (response) {
-				 return response;
-			 }
- 
-			 return fetch(event.request);
-		 })
-	 );
- });
- 
+var URLS = [
+	'/sass/main.min.css',
+	'/js/script.js',
+	'/images/mountain.svg',
+	'/images/balloon.svg',
+	'/',
+	'/android-chrome-192x192.png',
+	'/android-chrome-512x512.png',
+	'/apple-touch-icon.png',
+	'/favicon.ico',
+	'/favicon-16x16.png',
+	'/favicon-32x32.png',
+	'/favicon.svg',
+	'/manifest.json',
+	'/mstile-150x150.png',
+	'/safari-pinned-tab.svg'
+]
+
+this.addEventListener('install', function (event) {
+	event.waitUntil(
+		caches.open(CACHE_NAME).then(function (cache) {
+			return cache.addAll(URLS);
+		})
+	);
+});
+
+this.addEventListener('fetch', function (event) {
+	event.respondWith(
+		caches.match(event.request).then(function (response) {
+			if (response) {
+				return response;
+			}
+
+			return fetch(event.request);
+		})
+	);
+});
